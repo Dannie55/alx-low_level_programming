f6bd04f9509219a7803536f60fe7f031858e97ba
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-08 01:35:51 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * A programme that prints exactly folowed by a new line
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n")
+		return (0);
+}
