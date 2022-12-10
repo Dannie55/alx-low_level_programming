92669381b61e02336f0c79b05830fd40931cb267
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-09 22:41:08 -0800

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Author: Daniel
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char pam;
+
+	for (pam = 'a'; pam <= 'z'; pam++)
+	{
+		putchar(pam);
+	}
+	putchar('\n');
+		return (0);
+}
