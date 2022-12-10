55090509918c0bf55119964ac528621a4b15dc29
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-09 22:45:51 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Author: Daniel
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	for (d = 'A' ; d <= 'Z' ; d++)
+		putchar(d);
+	putchar ('\n');
+	return (0);
+}
