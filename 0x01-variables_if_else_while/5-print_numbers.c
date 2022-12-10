808705c0f5e0346005d3a7feb962351b960715e8
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-09 21:56:05 -0800

    5-printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * author: Daniel
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int dannie;
+
+	for (dannie = 0 ; dannie < 10 ; dannie++)
+	{
+		printf("%d", dannie);
+	}
+	printf("\n");
+	return (0);
+}
