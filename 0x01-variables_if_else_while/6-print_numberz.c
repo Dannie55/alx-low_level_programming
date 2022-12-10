a2d07a9ccc67c748dc918999bfe7ad8f0bfb26cd
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-09 22:01:50 -0800

    orintnmberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** main - Entry point
+ *  author: daniel
+ *  Return: Always 0 (success)
+ */
+int main(void)
+{
+	int dannie;
+
+	for(dannie = 48 ; dannie < 58 ; dannie++)
+		putchar(dannie);
+	putchar('\n');
+	return (0);
+}
