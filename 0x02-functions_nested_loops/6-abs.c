c13534db5ffa1d064ae30a8d3504e2015b8dd323
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-13 18:52:43 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * abs - computes the absolute value of an integer
+  * int abs - computes the absolute value of an integer
   * @n: the int to be operated upon
   * Return: returns an unsigned int value to calling function
  */
