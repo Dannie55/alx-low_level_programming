8c7b72db1d31a75a2d11a444054301789594f21c
Dannie55 <ezirimdaniel080@gmail.com>
2022-12-13 07:15:43 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,8 @@
   * abs - computes the absolute value of an integer
   * @n: the int to be operated upon
   * Return: returns an unsigned int value to calling function
-  */
+ */
+
 int _abs(int n)
 {
 	if (n >= 0)
