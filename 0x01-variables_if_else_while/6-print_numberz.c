523a28a2de639d67164c2dc0dc9c823f26862538
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-20 17:34:12 +0100

    Update 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Dannie
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dannie;
+
+	for (dannie = 48 ; dannie < 58 ; dannie++)
+		putchar(dannie);
+	putchar('\n');
+	return (0);
+}
