01547fd197c01e8583e487956215335951f7650d
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-20 17:29:25 +0100

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: IamNaeto
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char danny;
+
+	for (danny= 'a' ; danny <= 'z' ; danny++)
+	{
+		if (danny != 'q' && danny != 'e')
+			putchar(danny);
+	}
+	putchar('\n');
+	return (0);
+}
