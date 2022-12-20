a8b788fd893ed29357f81efba903c79d54031efe
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-20 17:26:31 +0100

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char dan;
+
+	for (dan = 'a'; dan <= 'z'; dan++)
+	{
+		putchar(dan);
+	}
+	putchar('\n');
+		return (0);
+}
