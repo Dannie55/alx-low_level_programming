65d7e22b9791bb2f89d3ac1695dbd21e072714ad
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-20 17:38:32 +0100

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * author: dannie
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int naeto;
+	char charlie;
+
+	for (dannie = 48 ; dannie < 58 ; dannie++)
+		putchar(dannie);
+	for (ezirim = 'a' ; ezirim <= 'f' ; ezirim++)
+		putchar(ezirim);
+	putchar('\n');
+	return (0);
+}
