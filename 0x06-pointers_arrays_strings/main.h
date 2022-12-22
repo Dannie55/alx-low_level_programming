b63e59dd7106c05d46b368855619d400412c55c7
EzirimDaniel <Ezirimdaniel080@gmail.com>
2022-12-22 01:36:13 -0800

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_touper(char *);
+char *string_toupper(char *);
 
 char *cap_string(char *);
 
@@ -28,4 +28,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
-
