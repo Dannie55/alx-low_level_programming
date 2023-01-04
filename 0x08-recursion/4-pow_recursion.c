44306c103b12102c75cb88a40812533be8c44c4b
Dannie55 <ezirimdaniel080@gmail.com>
2023-01-04 05:10:12 -0800

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *_pow_recursion- returns the value of @x raised to the power of @y
+  *_pow_recursion- returns the value of @x raised to the power of y
   *@x: number
   *@y: the power to raise @n to
   @Return: the value of @x to the @y power
