e72712a9c1b1085834690378d409f00ac2440945
EzirimDaniel <Ezirimdaniel080@gmail.com>
2023-01-07 02:23:45 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,46 +1,31 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
  *
- * Return: the int converted from the string
+ * Return: a pointer to the resulting string dest
  */
-int _atoi(char *s)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, d, n, len, f, digit;
+	int i, j;
 
 	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	j = 0;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
+	while (dest[i] != '\0')
 		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	if (f == 0)
-		return (0);
+	dest[i] = '\0';
 
-	return (n);
+	return (dest);
 }
