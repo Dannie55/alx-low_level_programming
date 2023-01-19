20da210931ad9bba4968ee8ec0c8a13d6aa72d9f
Dannie55 <ezirimdaniel080@gmail.com>
2023-01-19 10:59:12 -0800

    update

---
+++ 0x0F-funtion_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+  * main - program that performs simple operations
+  * @argc: number of arguments
+  * @argv: array of arguments
+  *
+  * Return: Always 0 (success)
+  */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2];
+
+	if (!func)
+	{
+		printf(
+	
