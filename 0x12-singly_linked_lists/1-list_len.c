f06894a9f7bbbf2763c477625fdf1ab76515cb56
Dannie55 <ezirimdaniel080@gmail.com>
2023-01-26 14:00:01 -0800

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,10 +3,10 @@
 #include "lists.h"
 
 /**
-  * list_len - displays the number of elementsof a list
-  * @h: a linked list
+  * list_len - show the no of elements of a list
+  * @h: A linked list
   *
-  * Return
+  * Return: The number of elements of a list
   */
 size_t list_len(const list_t *h)
 {
