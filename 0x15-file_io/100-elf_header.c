909b400af571fb10b166c69779bce4d4253ae9a9
EzirimDaniel <Ezirimdaniel080@gmail.com>
2023-02-07 08:13:21 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,4 @@
-#include <elf.h>
+No#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -312,4 +312,3 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	close_elf(o);
 	return (0);
 }
-x
