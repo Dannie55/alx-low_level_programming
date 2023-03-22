c21cc88abb71d9caa4dae401b5422e724a6bdce3
Dannie55 <ezirimdaniel080@gmail.com>
2023-03-22 00:10:45 -0700

    updated

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -9,22 +9,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	char *temp = dest;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (temp);
 }
