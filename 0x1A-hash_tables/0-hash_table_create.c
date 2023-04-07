eca3a88bfb4345a8ebad10e2602239b131057635
Dannie55 <ezirimdaniel080@gmail.com>
2023-04-07 06:02:15 -0700

    fixed bugs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,18 @@
   */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	hash_table_t *new_table = NULL;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (size == 0)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size)
-	if (ht->array == NULL)
+	new_table->size = size;
+	new_table->array = calloc((size_t) size, sizeof(hash_node_t *));
+	if (new_table->array == NULL)
 		return (NULL);
-	for (i = 0, i < size, i++)
-		ht->array[i] = NULL;
 
-	return (ht);
+	return (new_table);
 }
