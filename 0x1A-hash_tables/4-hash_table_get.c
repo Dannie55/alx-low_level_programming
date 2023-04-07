70029279f7056a9408f4d5111a362223af35e00b
Dannie55 <ezirimdaniel080@gmail.com>
2023-04-07 06:33:33 -0700

    fixedbugs

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,14 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_get - retrieves a value associated with a key.
-  * @ht:A pointer to the table
-  * @key: The key to obtain the value.
-  *
-  * Return: if the key cannot be found return NULL
-  */
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ *
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
